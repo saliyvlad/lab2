f3fdbb02a6676fabf84170a25c19daddcadb683b
saliyvlad <saliy.vlad.jr@gmail.com>
2024-11-12 07:43:06 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,29 @@
-#include <stdio.h>
+#!/bin/bash
 
-void main(){
-	printf("Hello, world!\n");
-}
+# Ввод названия каталога
+read -p "Введите название каталога: " dirname
+
+# Проверка существования каталога
+if [ -d "$dirname" ]; then
+    echo "Каталог $dirname существует."
+else
+    echo "Каталог $dirname не существует."
+    exit 1
+fi
+
+# Поиск файла, имя которого начинается с символа 'a'
+file=$(find "$dirname" -type f -name "a*")
+if [ -z "$file" ]; then
+    echo "Файл, начинающийся с 'a', не найден."
+    exit 1
+fi
+
+echo "Найден файл: $file"
+
+# Замена знаков пунктуации на '#'
+tr -c '[:alnum:][:space:]' '#' < "$file" > "${file%.txt}.UP"
+
+# Сохранение в каталог Z54
+mv "${file%.txt}.UP" "Z54/"
+
+echo "Преобразованный файл сохранен в каталоге Z54 с именем ${file##*/}.UP"
