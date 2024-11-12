49201c9317221bacead34a7058f58c71d019156e
saliyvlad <saliy.vlad.jr@gmail.com>
2024-11-12 07:54:54 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -27,3 +27,33 @@ tr -c '[:alnum:][:space:]' '#' < "$file" > "${file%.txt}.UP"
 mv "${file%.txt}.UP" "Z54/"
 
 echo "Преобразованный файл сохранен в каталоге Z54 с именем ${file##*/}.UP"
+
+
+
+
+#!/bin/bash
+
+# Ввод названия каталога
+read -p "Введите название каталога: " dir_name
+
+# Проверка существования каталога
+if [ -d "$dir_name" ]; then
+    echo "Каталог существует."
+else
+    echo "Каталог не существует."
+    exit 1
+fi
+
+# Поиск файла, начинающегося с 'a'
+file=$(find "$dir_name" -type f -name 'a*' | head -n 1)
+
+if [ -z "$file" ]; then
+    echo "Файл не найден."
+    exit 1
+fi
+
+# Замена знаков пунктуации на '#'
+new_file="${file%.txt}.UP"  # Предполагается, что файл имеет расширение .txt
+tr -c '[:alnum:][:space:]' '#' < "$file" > "$new_file"
+
+echo "Преобразованный файл сохранен как $new_file."
